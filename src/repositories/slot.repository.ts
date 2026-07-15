4c309be1dacfeee511f7a0342b8b2b3156cf50b0
Joy Dutta <dutta_joy@hotmail.com>
2026-07-15 19:48:17 +0100

    Adds more repository methods: slot

---
+++ src/repositories/slot.repository.ts
@@ -0,0 +1,13 @@
+import {prisma} from "../config/database.js"
+export async function findBookedSlotsByHostInRange(hostId: number, startDate: Date, endDate: Date) {
+    await prisma.slot.findMany({
+        where: {
+            hostId, 
+            startAt: {
+                gte: startDate,
+                lte: endDate
+            },
+            status: "BOOKED"
+        }
+    })
+}
